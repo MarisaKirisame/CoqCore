967bda3735e3aa0dc4631e7f7fc1fc1f829cc5ea
lolisa <lolisa@marisa.moe>
2015-07-30 09:27:41 -0700

    Move EqDec. Add compile script.

---
+++ EqDec.v
@@ -0,0 +1,14 @@
+Definition eq_dec T := forall l r : T, { l = r } + { l <> r }.
+
+Ltac dedec dec :=
+  match goal with
+  | |- ?Y => 
+      match Y with 
+        context f [dec ?l ?r] => destruct (dec l r)
+      end
+  | X : _ |- _ => 
+      match type of X with
+      | context [dec ?l ?r] =>
+          destruct (dec l r)
+      end
+  end.
