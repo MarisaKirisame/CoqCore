0f75f982227188693eee65a7de0561ec6b1c3468
lolisa <lolisa@marisa.moe>
2015-09-08 15:13:32 -0700

    Refactor code.

--- MoreJMeq.v
+++ MoreJMeq.v
@@ -4,14 +4,20 @@ Set Implicit Arguments.
 Create HintDb MoreJMeq discriminated.
 Hint Extern 1 => progress subst : MoreJMeq.
 
-Hint Extern 1 => apply subset_eq : MoreJMeq.
+Definition eq_proj1_sig_eq (A : Type) (P : A -> Prop) (n m : {x : A | P x}) : n = m -> ` n = ` m
+  := $(intuition)$.
+Hint Resolve functional_extensionality eq_proj1_sig_eq : MoreJMeq.
+
+Definition proj1_sig_eq_eq (A : Type) (P : A -> Prop) (n m : {x : A | P x}) : ` n = ` m -> n = m
+  := $(intros; apply subset_eq; trivial)$.
+Hint Resolve proj1_sig_eq_eq : MoreJMeq.
 
 Definition eq_JMeq T (l r : T) : l = r -> l ~= r := $(auto with MoreJMeq)$.
-
-Hint Resolve eq_JMeq functional_extensionality : MoreJMeq.
+Hint Resolve eq_JMeq : MoreJMeq.
 
 Definition JMeq_extensionality_l A B C (l : A -> C) (r : B -> C) :
   (forall L R, L ~= R -> l L = r R) -> A = B -> l ~= r := $(eauto with MoreJMeq)$.
+Hint Resolve JMeq_extensionality_l : MoreJMeq.
 
 Theorem JMeq_extensionality_r A B C (l : A -> B) (r : A -> C) :
   (forall L R, L = R -> l L ~= r R) -> B = C -> l ~= r.
@@ -19,6 +25,7 @@ Theorem JMeq_extensionality_r A B C (l : A -> B) (r : A -> C) :
   apply eq_JMeq; apply functional_extensionality.
   intros; erewrite H; eauto.
 Qed.
+Hint Resolve JMeq_extensionality_r : MoreJMeq.
 
 Theorem JMeq_extensionality A B C D (l : A -> B) (r : C -> D) :
   (forall L R, L ~= R -> l L ~= r R) -> A = C -> B = D -> l ~= r.
@@ -26,6 +33,17 @@ Theorem JMeq_extensionality A B C D (l : A -> B) (r : C -> D) :
   apply eq_JMeq; apply functional_extensionality.
   intros; erewrite H; eauto.
 Qed.
+Hint Resolve JMeq_extensionality : MoreJMeq.
 
-Definition subset_JMeq T (A B : T -> Prop) (l : sig A) (r : sig B) :
-  A = B -> l ~= r <-> ` l = ` r := $(intuition)$.
+Definition JMeq_proj1_eq T (A B : T -> Prop) (l : sig A) (r : sig B) :
+  A = B -> ` l = ` r -> l ~= r .
+  intros; subst.
+  apply eq_JMeq.
+  eauto with MoreJMeq.
+  eauto using proj1_sig_eq_eq.
+Qed.
+
+Definition eq_JMeq_proj1_eq T (A B : T -> Prop) (l : sig A) (r : sig B) :
+  A = B -> l ~= r -> ` l = ` r := $(intuition)$.
+
+Hint Resolve JMeq_proj1_eq eq_JMeq_proj1_eq : MoreJMeq.
