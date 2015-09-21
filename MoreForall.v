b24d8ea378a4ac0afc6280fe51a9a7ea0d9b2875
lolisa <lolisa@marisa.moe>
2015-09-21 15:26:56 -0700

    add in_in_map

--- MoreForall.v
+++ MoreForall.v
@@ -6,36 +6,43 @@ Theorem Forall_split T P (a : T) l : Forall P (a::l) <-> P a /\ Forall P l.
   intuition;inversion H;tauto.
 Qed.
 
-Definition Forall_map A B (P : A -> Prop) (f : forall a, P a -> B) :
-  forall(l : list A), Forall P l -> list B.
-  induction l;
-  intros.
-  exact [].
-  apply Forall_split in H.
-  destruct H.
-  specialize(IHl H0).
-  exact ((f _ H)::IHl).
-Defined.
+Program Fixpoint In_map { A B } (l : list A) (f : forall a, In a l -> B) : list B := 
+  match l with
+  | [] => []
+  | h :: t => f h _ :: In_map t (fun a _ => f a _)
+  end.
+Solve All Obligations with program_simpl; simpl; tauto.
+
+Program Definition Forall_map A B (P : A -> Prop) (f : forall a, P a -> B) :
+  forall (l : list A), Forall P l -> list B := fun l H => In_map l (fun a _ => f a _).
+Next Obligation.
+  eapply Forall_forall; eauto.
+Qed.
+
+Theorem in_In_map' { A B : Type } l e : 
+  forall f f' P, f = f' -> In (f e P) (@In_map A B l f').
+  induction l; intros; simpl in *; ii.
+  inversion P; subst.
+  left; f_equal; apply proof_irrelevance.
+  right; pose (fun x p => f' x (or_intror p)) as h.
+  replace (f' e P) with (h e H0) by (unfold h; f_equal; apply proof_irrelevance); auto.
+Qed.
+
+Definition in_In_map A B l e f P : In (f e P) (@In_map A B l f) := 
+  @in_In_map' A B l e f f P eq_refl.
 
 Theorem in_Forall_map A B (P : A -> Prop) (f : forall a, P a -> B) l F e p : 
   In e l -> In (f e p) (@Forall_map A B P f l F).
-  induction l;
-  intros;
-  simpl in *.
-  tauto.
-  destruct Forall_split.
-  destruct a0.
-  intuition;simpl;subst.
-  left.
-  f_equal;apply proof_irrelevance.
+  unfold Forall_map; intros.
+  pose (fun e H => f e (proj1 (Forall_forall P l) F e H)) as g.
+  replace (f e p) with (g e H) by (unfold g; f_equal; apply proof_irrelevance).
+  apply in_In_map'; unfold g; extensionality a; extensionality H0.
+  f_equal; apply proof_irrelevance.
 Qed.
 
 Theorem Forall_ignore_left T P (l r : list T) : Forall P (l ++ r) -> Forall P r.
-  induction l.
-  trivial.
-  simpl.
-  inversion 1.
-  tauto.
+  induction l; simpl; trivial.
+  inversion 1; tauto.
 Qed.
 
 Theorem Forall_perm T P (l r : list T) : Permutation l r -> Forall P l -> Forall P r.
@@ -51,21 +58,20 @@ Qed.
 Theorem Forall_ignore_right T P (l r : list T) : Forall P (l ++ r) -> Forall P l.
   intros.
   assert(Permutation (l ++ r) (r ++ l)) by apply Permutation_app_comm.
-  eapply Forall_perm in H0;eauto.
-  eapply Forall_ignore_left;eauto.
+  eapply Forall_perm in H0; eauto.
+  eapply Forall_ignore_left; eauto.
 Qed.
 
 Theorem Forall_app T P (l r : list T) : Forall P (l ++ r) <-> Forall P l /\ Forall P r.
   intuition.
-  eapply Forall_ignore_right;eauto.
-  eapply Forall_ignore_left;eauto.
+  eapply Forall_ignore_right; eauto.
+  eapply Forall_ignore_left; eauto.
   induction l;
   repeat match goal with
   | H : Forall _ (_ :: _) |- _ => invcs H
   | |- Forall _ (_ :: _) => constructor
-  end;
-  simpl;
-  intuition.
+  end; 
+  simpl; intuition.
 Qed.
 
 Ltac ForallInvcs := 
