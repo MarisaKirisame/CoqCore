df363c0e461b31ce556e0bd403649a0739fd4a1d
lolisa <lolisa@marisa.moe>
2015-12-21 00:56:26 -0800

    refactor code

--- Tactic.v
+++ Tactic.v
@@ -40,32 +40,31 @@ Ltac match_type_context_destruct T C :=
   let F := (fun x => let x' := type of x in no_inner_match_smart_destruct) in
     get_match C F.
 
-Definition box P NT (N : NT) : Type := P.
+Definition channel P NT (N : NT) : Type := P.
+
+Arguments channel : simpl never.
 
 Inductive sandbox_closer : Prop := ms : sandbox_closer -> sandbox_closer.
 
-Theorem sandbox_closer_exit : sandbox_closer -> False. induction 1;trivial. Qed.
+Definition sandbox_closer_exit : sandbox_closer -> False := $(induction 1; trivial)$.
 
-Arguments box : simpl never.
+Ltac set_result N T := match goal with | _ := ?X : channel _ N |- _ => unify X T end.
+
+Ltac get_result N := match goal with | _ := ?X : channel _ N |- _ => X end.
+
+Ltac clear_result N := match goal with | H := _ : channel _ N |- _ => clear H end.
 
 Ltac make_sandbox T N := 
-  let f := fresh in
-    evar (f : box T N);
-    let g := get_goal in 
-      let H := fresh in
-        assert(sandbox_closer -> g) as H;[intro|clear H].
+  let f := fresh in 
+  let g := get_goal in 
+  let H := fresh in
+    evar (f : channel T N); assert(sandbox_closer -> g) as H; [intro | clear H].
 
 Ltac exit_sandbox := 
   exfalso;
   match goal with
-  | X : sandbox_closer |- _ => apply sandbox_closer_exit in X;tauto
+  | X : sandbox_closer |- _ => apply sandbox_closer_exit in X; tauto
   end.
-
-Ltac set_result N T := match goal with | _ := ?X : box _ N |- _ => unify X T end.
-
-Ltac get_result N := match goal with | _ := ?X : box _ N |- _ => X end.
-
-Ltac clear_result N := match goal with | H := _ : box _ N |- _ => clear H end.
 
 Ltac match_type_destruct T :=
   let F := (fun x => 
